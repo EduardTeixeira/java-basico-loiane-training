e91c5cdb7579d21aca52839f87f746538192c6cb
Eduard Teixeira <eduard.teixeira01@gmail.com>
2021-12-30 12:08:51 -0300

    passando argumentos para o programa

---
+++ aula06/src/Argumentos.java
@@ -0,0 +1,13 @@
+package aula06;
+
+public class Argumentos {
+
+	public static void main(String[] args) {
+
+		for (String value : args) {
+			System.out.println("Você digitou " + value);
+		}
+
+	}
+
+}
