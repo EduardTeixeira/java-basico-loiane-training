32617a02855e4c8edd1bba4e020b1d92c714abc7
Eduard Teixeira <eduard.teixeira01@gmail.com>
2022-02-01 14:50:01 -0300

    aula 10 - variaveis

---
+++ curso-java-basico/src/com/treinamento/cursojava/aula10/Variaveis.java
@@ -0,0 +1,24 @@
+package com.treinamento.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		// Declarações Corretas no Java
+		int idade = 10;
+		String nome = "nome";
+		String nomeDoMeuCachorro = "cachorro";
+
+		// Declarações coretas porém não é convenção java
+		int _idade;
+		int $idade;
+		String ano2022;
+		String nome_do_meu_cachorro;
+
+		idade += 2;
+
+		System.out.println("Idade = " + idade);
+
+	}
+
+}
